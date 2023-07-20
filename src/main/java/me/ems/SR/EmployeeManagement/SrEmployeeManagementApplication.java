d55299be0ae00718e231fdcdee7a0631f8a2823f
HASHAN <hashanmadhuwantha2000@gmail.com>
2023-07-20 11:27:03 +0530

    Initial commit

---
+++ src/main/java/me/ems/SR/EmployeeManagement/SrEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package me.ems.SR.EmployeeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrEmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ems/SR/EmployeeManagement/SrEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ems.SR.EmployeeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrEmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
